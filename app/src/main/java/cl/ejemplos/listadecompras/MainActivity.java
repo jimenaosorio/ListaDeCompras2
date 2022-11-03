6a4d1aaa00939ce11d04dd06001bdc5c4cc12fee
Jimena <jimena.b.osorio@gmail.com>
2022-11-03 19:33:22 -0300

    Lista de Compras UMAG 2022, con ArrayList

---
+++ app/src/main/java/cl/ejemplos/listadecompras/MainActivity.java
@@ -0,0 +1,59 @@
+package cl.ejemplos.listadecompras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import cl.ejemplos.listadecompras.modelo.ListaDeCompras;
+import cl.ejemplos.listadecompras.modelo.Producto;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button verLista=(Button) findViewById(R.id.ver_lista);
+        verLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<Producto> productos= ListaDeCompras.getInstancia().getListaDeCompras();
+                if(productos.size() > 0)
+                {
+                    Intent intent=new Intent(MainActivity.this,ListaProductosActivity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "La lista de compras está vacía",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        Button botonNuevo=(Button) findViewById(R.id.botonNuevo);
+        botonNuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, NuevoProductoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button botonEliminar=(Button) findViewById(R.id.botonEliminar);
+        botonEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ListaDeCompras.getInstancia().eliminarComprados();
+                Toast.makeText(MainActivity.this,"Se han eliminado los productos comprados",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
